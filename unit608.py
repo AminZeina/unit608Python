c897a6e33e33961d3aae40cbf8489b3267800847
Amin Zeina <36240585+CitrusMelon@users.noreply.github.com>
2018-04-09 16:19:06 -0400

    Initial commit

---
+++ unit608.py
@@ -0,0 +1,15 @@
+# Created By: Amin Zeina 	
+# Created On: Apr 2018
+
+# Multiplication
+
+
+answer = 0
+number_a = int(input('Enter your first number: '))
+number_b = int(input('Enter your second number: '))
+
+for i in range(0,number_b):
+	answer = answer + number_a
+
+print('Your answer is {}' .format(answer))
+input('Program End.')
